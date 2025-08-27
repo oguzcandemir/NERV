b7f722e8eff4c9e97224e14d340c20e7112dd081
Majaahh <maja.blaszczak@proton.me>
2025-08-27 21:40:37 +0200

    unica: patches: mass_cam: Switch to camera app from a73xqxx
    Change-Id: I30ce9adbfabfb43f74cf0347275fa252cc6e9cdc
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- unica/patches/mass_cam/customize.sh
+++ unica/patches/mass_cam/customize.sh
@@ -6,7 +6,7 @@ if ! $SOURCE_HAS_MASS_CAMERA_APP; then
         if [ -f "$WORK_DIR/system/system/priv-app/SamsungCamera/SamsungCamera.apk.prof" ]; then
             DELETE_FROM_WORK_DIR "system" "system/priv-app/SamsungCamera/SamsungCamera.apk.prof"
         fi
-        ADD_TO_WORK_DIR "r11sxxx" "system" "system/priv-app/SamsungCamera/SamsungCamera.apk" 0 0 644 "u:object_r:system_file:s0"
+        ADD_TO_WORK_DIR "a73xqxx" "system" "system/priv-app/SamsungCamera/SamsungCamera.apk" 0 0 644 "u:object_r:system_file:s0"
     else
         LOG "- TARGET_HAS_MASS_CAMERA_APP is not set. Ignoring"
     fi
